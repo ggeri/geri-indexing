b76d2edd592c9ff710cb6556e9da3e48b66ead5a
g.grolinger <g.grolinger@4e4a4f93-7246-0410-8c94-f18fd6689ace>
2008-09-19 00:39:08 +0000

    Got rid of the package structure.

--- trunk/src/ca/utoronto/cs/kdtree/basic/Node.java
+++
@@ -1,7 +0,0 @@
-package ca.utoronto.cs.kdtree.basic;
-import java.io.Serializable;
-
-public abstract class Node implements Serializable
-{
-
-}
--- trunk/src/ca/utoronto/cs/kdtree/groundtruth/Node.java
+++
@@ -1,7 +0,0 @@
-package ca.utoronto.cs.kdtree.groundtruth;
-import java.io.Serializable;
-
-public abstract class Node implements Serializable
-{
-
-}
