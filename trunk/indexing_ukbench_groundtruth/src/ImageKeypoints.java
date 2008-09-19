9c4f16adf7caf70f23fe7d4986221df1dc6634e3
g.grolinger <g.grolinger@4e4a4f93-7246-0410-8c94-f18fd6689ace>
2008-09-19 01:39:09 +0000

    Added a print statement for debugging.

--- trunk/indexing_ukbench_groundtruth/src/ImageKeypoints.java
+++ trunk/indexing_ukbench_groundtruth/src/ImageKeypoints.java
@@ -224,12 +224,18 @@ public class ImageKeypoints implements Serializable
 			for(int j = 0; j < desc.length; j++)
 			{
 				magn = magn + desc[j];
+			}			
+			
+			try {
+				for(int j = 0; j < desc.length; j++)
+				{
+					desc[j] = (short) Math.floor(desc[j] / magn);
+				}
+			} catch (Exception e) {
+				System.out.println("magn: " + magn);
+				e.printStackTrace();
 			}
-			System.out.println("magn: " + magn);
-			for(int j = 0; j < desc.length; j++)
-			{
-				desc[j] = (short) Math.floor(desc[j] / magn);
-			}
+			
 			keypt.setDescriptor(desc);
 		}
 	}
